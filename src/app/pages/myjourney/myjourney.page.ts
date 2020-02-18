f908ff2a1d56adc53d951cd0aebed203d397431d
Muro96 <fCeS5cZX4yV28>
2020-02-18 11:28:40 +0100

    fix bug dissapear label on dep and arr

--- src/app/pages/myjourney/myjourney.page.ts
+++ src/app/pages/myjourney/myjourney.page.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { ModalController, Platform } from '@ionic/angular';
 import { Ionic4DatepickerModalComponent } from '@logisticinfotech/ionic4-datepicker';
 import { DatabaseService, Client } from 'src/app/database.service';
 import { LatLng } from 'leaflet';
+
 declare var google;
 @Component({
   selector: 'app-myjourney',
@@ -11,11 +13,16 @@ declare var google;
 })
 export class MyjourneyPage implements OnInit {
     
-  selectedView = 'list_journey';
+  selectedView = 'add_journey';
+  map:any;
+  @ViewChild('map',{static:false}) maq;
   
   time: string = new Date().toLocaleTimeString('it-IT',{hour: '2-digit', minute:'2-digit'});
   options = {day: 'numeric', month: 'numeric', year:'numeric'};
-  
+
+
+  input_value : any;
+  input_value1 : any;
   mydate : string = new Date().toLocaleDateString("it-IT",this.options);
 
   datePickerObj: any = {};
@@ -39,9 +46,17 @@ export class MyjourneyPage implements OnInit {
   selectedDate: any;
   clients : Client[] = [];
 
-  constructor(public modalCtrl: ModalController,private database: DatabaseService) {}
+  constructor(public modalCtrl: ModalController,private database: DatabaseService,private platform:Platform) {
+  }
 
-  ngOnInit() {
+   ngOnInit() {
+    this.platform.ready().then(() =>{
+      this.map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 45.63488, lng:  12.57211},
+        zoom: 13,
+        mapTypeId: 'roadmap'
+      });  
+    });
     this.datePickerObj = {
       inputDate: this.mydate,
       dateFormat: 'DD/MM/YYYY',
@@ -63,6 +78,7 @@ export class MyjourneyPage implements OnInit {
       }
     });
   }
+ 
 
   onChangeDate() {
     console.log('onChangeDate date ', this.mydate);
@@ -94,6 +110,7 @@ export class MyjourneyPage implements OnInit {
   getAddressDep(){
     let place: { geometry: { location: { lat: () => void; lng: () => any; }; }; };
     let inputfield = document.getElementById('autocomplete_input_dep').getElementsByTagName('input')[0];
+    this.input_value = inputfield.value;
     let autocomplete_dep = new google.maps.places.Autocomplete((inputfield), {
       types:['address'],
       componentRestrictions: {country: 'it'},
@@ -102,6 +119,13 @@ export class MyjourneyPage implements OnInit {
       var place = autocomplete_dep.getPlace();
       var myLatlng = new google.maps.LatLng(place.geometry.location.lat(),place.geometry.location.lng());
       console.log("address_Dep"+myLatlng);
+      let marker = new google.maps.Marker(
+        {
+              map: this.map,
+              draggable: true,
+              animation: google.maps.Animation.DROP,
+              position: myLatlng
+        });
       return myLatlng;
       
     });
@@ -109,6 +133,8 @@ export class MyjourneyPage implements OnInit {
   getAddressArr(){
     let place: { geometry: { location: { lat: () => void; lng: () => any; }; }; };
     let inputfield = document.getElementById('autocomplete_input_arr').getElementsByTagName('input')[0];
+    this.input_value1 = inputfield.value;
+
     let autocomplete_arr = new google.maps.places.Autocomplete((inputfield), {
       types:['address'],
       componentRestrictions: {country: 'it'},
@@ -117,13 +143,28 @@ export class MyjourneyPage implements OnInit {
       place = autocomplete_arr.getPlace();
       var myLatlng = new google.maps.LatLng(place.geometry.location.lat(),place.geometry.location.lng());
       console.log("address_Arr"+myLatlng);
+      let marker = new google.maps.Marker(
+        {
+              map: this.map,
+              draggable: true,
+              animation: google.maps.Animation.DROP,
+              position: myLatlng
+        });
+      console.log("markerrr"+marker);
       return myLatlng;
   });
 
   
   }
 
+  async initMap(){
+    //const pos1: LatLng = this.getAddressArr();
+    //const pos2: LatLng = this.getAddressDep();
+  }
+    
+  }
 
-}
+
+
 
  
